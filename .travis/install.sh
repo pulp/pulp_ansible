d4509f17a46a5047ddf6d5f7517915a654d4279a
Dennis Kliban <dkliban@redhat.com>
2019-02-28 15:22:35 -0500

    Switches Travis to use the new pulpcore repo
    https://pulp.plan.io/issues/4444

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,21 +2,21 @@
 set -v
 
 export COMMIT_MSG=$(git show HEAD^2 -s)
-export PULP_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:\/\/github\.com\/pulp\/pulp\/pull\/(\d+)' | awk -F'/' '{print $7}')
+export PULP_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:\/\/github\.com\/pulp\/pulpcore\/pull\/(\d+)' | awk -F'/' '{print $7}')
 export PULP_PLUGIN_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:\/\/github\.com\/pulp\/pulpcore-plugin\/pull\/(\d+)' | awk -F'/' '{print $7}')
 
 pip install -r test_requirements.txt
 
-cd .. && git clone https://github.com/pulp/pulp.git
+cd .. && git clone https://github.com/pulp/pulpcore.git
 
 if [ -n "$PULP_PR_NUMBER" ]; then
-  pushd pulp
+  pushd pulpcore
   git fetch origin +refs/pull/$PULP_PR_NUMBER/merge
   git checkout FETCH_HEAD
   popd
 fi
 
-pip install -e ./pulp
+pip install -e ./pulpcore
 
 git clone https://github.com/pulp/pulpcore-plugin.git
 
