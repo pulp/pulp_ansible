8f54e477481ebc15b605adeac56c8f30cbd6520b
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-09-17 18:10:04 -0300

    Fix OpenAPI pagination
    [noissue]
    
    (cherry picked from commit b4e9b484e856d4813888e471ac50c504c6d6ae63)

--- pulp_ansible/app/galaxy/v3/serializers.py
+++ pulp_ansible/app/galaxy/v3/serializers.py
@@ -1,5 +1,7 @@
 from gettext import gettext as _
 from django.conf import settings
+from drf_spectacular.utils import extend_schema_field
+from drf_spectacular.types import OpenApiTypes
 from rest_framework.reverse import reverse
 from rest_framework import serializers, relations
 
@@ -53,6 +55,7 @@ class CollectionSerializer(serializers.ModelSerializer):
             kwargs={"path": self.context["path"], "namespace": obj.namespace, "name": obj.name},
         )
 
+    @extend_schema_field(OpenApiTypes.OBJECT)
     def get_highest_version(self, obj):
         """Get a highest version and its link."""
         href = reverse(
