7c2a2dc163f93c7e34ae8208eb55c85d032f3d78
Brian Bouterse <bmbouter@redhat.com>
2020-11-05 15:49:02 -0500

    Adds validation to `CollectionRemoteSerializer`
    When using either the `token` or `auth_url` attributes, ensure the user
    is using both.
    
    closes #7802

---
+++ pulp_ansible/tests/functional/api/collection/test_remote.py
@@ -0,0 +1,34 @@
+"""Tests related to CollectionRemote objects."""
+import unittest
+
+from pulpcore.client.pulp_ansible import RemotesCollectionApi
+from pulpcore.client.pulp_ansible.exceptions import ApiException
+
+from pulp_ansible.tests.functional.utils import gen_ansible_client, gen_ansible_remote
+from pulp_ansible.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
+
+
+class CollectionRemoteCase(unittest.TestCase):
+    """Test CollectionRemote."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Create class-wide variables."""
+        cls.client = gen_ansible_client()
+        cls.remote_collection_api = RemotesCollectionApi(cls.client)
+
+    def test_remote_with_url_only_is_allowed(self):
+        """Assert that a `CollectionRemote` with only a url can be created."""
+        body = gen_ansible_remote(url="https://example.com/")
+        remote = self.remote_collection_api.create(body)
+        self.addCleanup(self.remote_collection_api.delete, remote.pulp_href)
+
+    def test_token_requires_auth_url(self):
+        """Assert that a `CollectionRemote` with `token` and no `auth_url` can't be created."""
+        body = gen_ansible_remote(url="https://example.com/", token="this is a token string")
+        self.assertRaises(ApiException, self.remote_collection_api.create, body)
+
+    def test_auth_url_requires_token(self):
+        """Assert that a `CollectionRemote` with `auth_url` and no `token` can't be created."""
+        body = gen_ansible_remote(url="https://example.com/", auth_url="https://example.com")
+        self.assertRaises(ApiException, self.remote_collection_api.create, body)
