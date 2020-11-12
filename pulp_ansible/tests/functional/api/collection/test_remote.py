bf8703a4787f9e592c604e4c936a11a71c2f46d1
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-11-12 09:35:53 -0300

    Ensure that when creating a CollectionRemote with auth_url that you use  token together.
    https://pulp.plan.io/issues/7821
    closes #7821

--- pulp_ansible/tests/functional/api/collection/test_remote.py
+++ pulp_ansible/tests/functional/api/collection/test_remote.py
@@ -23,10 +23,11 @@ class CollectionRemoteCase(unittest.TestCase):
         remote = self.remote_collection_api.create(body)
         self.addCleanup(self.remote_collection_api.delete, remote.pulp_href)
 
-    def test_token_requires_auth_url(self):
-        """Assert that a `CollectionRemote` with `token` and no `auth_url` can't be created."""
+    def test_token_only_is_allowed(self):
+        """Assert that a `CollectionRemote` with `token` and no `auth_url` can be created."""
         body = gen_ansible_remote(url="https://example.com/", token="this is a token string")
-        self.assertRaises(ApiException, self.remote_collection_api.create, body)
+        remote = self.remote_collection_api.create(body)
+        self.addCleanup(self.remote_collection_api.delete, remote.pulp_href)
 
     def test_auth_url_requires_token(self):
         """Assert that a `CollectionRemote` with `auth_url` and no `token` can't be created."""
