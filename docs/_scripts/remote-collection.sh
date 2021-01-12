ef57539e15fe39515082355c427d1fe6f9cedc3f
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-01-12 10:44:01 -0300

    Testing collection worflows docs
    [noissue]

--- docs/_scripts/remote-collection.sh
+++ docs/_scripts/remote-collection.sh
@@ -1,11 +1,11 @@
 # Create a remote that syncs some versions of django into your repository.
 http POST $BASE_ADDR/pulp/api/v3/remotes/ansible/collection/ \
-    name='bar' \
+    name='cbar' \
     url='https://galaxy-dev.ansible.com/' \
     requirements_file:='"collections:\n  - testing.ansible_testing_content"'
 
 # Export an environment variable for the new remote URI.
-export REMOTE_HREF=$(http $BASE_ADDR/pulp/api/v3/remotes/ansible/collection/ | jq -r '.results[] | select(.name == "bar") | .pulp_href')
+export REMOTE_HREF=$(http $BASE_ADDR/pulp/api/v3/remotes/ansible/collection/ | jq -r '.results[] | select(.name == "cbar") | .pulp_href')
 
 # Lets inspect our newly created Remote
 http $BASE_ADDR$REMOTE_HREF
