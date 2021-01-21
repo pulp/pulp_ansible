9dd1429ab506faa6936976a12224a6232c93d10a
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-01-21 18:57:24 -0300

    Update GHA
    [noissue]

--- .ci/scripts/schema.py
+++ .ci/scripts/schema.py
@@ -1,3 +1,12 @@
+"""
+Customizing OpenAPI validation.
+
+OpenAPI requires paths to start with slashes:
+https://spec.openapis.org/oas/v3.0.3#patterned-fields
+
+But some pulp paths start with curly brackets e.g. {artifact_href}
+This script modifies drf-spectacular schema validation to accept slashes and curly brackets.
+"""
 import json
 from drf_spectacular.validation import JSON_SCHEMA_SPEC_PATH
 
