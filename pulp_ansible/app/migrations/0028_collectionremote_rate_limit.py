e960e9358d7f758c15ecc3b9cc37fcd3c8bd3ef1
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-01-25 19:08:06 -0300

    Make collection namespace max_length consistent in models
    Make CollectionVersion.namespace max_length match Collection.namespace
    
    https://pulp.plan.io/issues/8078
    closes #8078

--- pulp_ansible/app/migrations/0028_collectionremote_rate_limit.py
+++ pulp_ansible/app/migrations/0028_collectionremote_rate_limit.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('ansible', '0027_auto_20201120_1435'),
+        ('ansible', '0027_tag_length'),
     ]
 
     operations = [
