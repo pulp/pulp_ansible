cee276a6b3a35aeca1b2bdc904284f301aeafcc0
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-03-08 14:40:18 -0300

    Allow updating auth_url on CollectionRemote when token is already set
    https://pulp.plan.io/issues/8362
    closes #8362
    
    (cherry picked from commit 21b0777361183e06ba5647f484528d05612c9bf5)

--- pulp_ansible/tests/functional/api/collection/test_remote.py
+++ pulp_ansible/tests/functional/api/collection/test_remote.py
@@ -3,6 +3,7 @@ import unittest
 
 from pulpcore.client.pulp_ansible import RemotesCollectionApi
 from pulpcore.client.pulp_ansible.exceptions import ApiException
+from pulp_smash.pulp3.bindings import monitor_task
 
 from pulp_ansible.tests.functional.utils import gen_ansible_client, gen_ansible_remote
 from pulp_ansible.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
@@ -27,6 +28,22 @@ class CollectionRemoteCase(unittest.TestCase):
         """Assert that a `CollectionRemote` with `token` and no `auth_url` can be created."""
         body = gen_ansible_remote(url="https://example.com/", token="this is a token string")
         remote = self.remote_collection_api.create(body)
+        self.addCleanup(self.remote_collection_api.delete, remote.pulp_href)
+
+    def test_update_auth_url(self):
+        """Assert that a `CollectionRemote` with `token` and no `auth_url` can be created."""
+        body = gen_ansible_remote(
+            url="https://example.com/",
+            token="this is a token string",
+            auth_url="https://example.com",
+        )
+        remote = self.remote_collection_api.create(body)
+        response = self.remote_collection_api.partial_update(remote.pulp_href, {"auth_url": None})
+        monitor_task(response.task)
+        response = self.remote_collection_api.partial_update(
+            remote.pulp_href, {"auth_url": "https://example.com"}
+        )
+        monitor_task(response.task)
         self.addCleanup(self.remote_collection_api.delete, remote.pulp_href)
 
     def test_auth_url_requires_token(self):
