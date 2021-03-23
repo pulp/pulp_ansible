c280f604a9222bff7b1cdd6e6433be49bdebdeb7
Gerrod Ubben <gerrodubben@gmail.com>
2021-03-23 18:22:15 -0400

    Lowered functional tests rate limit
    [noissue]

--- pulp_ansible/tests/functional/utils.py
+++ pulp_ansible/tests/functional/utils.py
@@ -63,7 +63,7 @@ def gen_ansible_remote(url=ANSIBLE_FIXTURE_URL, **kwargs):
     :param url: The URL of an external content source.
     """
     if "rate_limit" not in kwargs:
-        kwargs["rate_limit"] = 10
+        kwargs["rate_limit"] = 5
 
     return gen_remote(url, **kwargs)
 
