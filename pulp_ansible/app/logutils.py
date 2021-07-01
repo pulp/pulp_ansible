633a3b0bd1c7aec76fce85d0ca269648925886bc
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-07-01 15:59:32 -0300

    Replace get_current_job with Task.current
    closes #9006
    
    (cherry picked from commit 09778afe0894017050783f19085a8f6a9b831266)

--- pulp_ansible/app/logutils.py
+++ pulp_ansible/app/logutils.py
@@ -1,7 +1,5 @@
 import logging
 
-from rq.job import get_current_job
-
 
 class CollectionImportHandler(logging.Handler):
     """
@@ -19,7 +17,8 @@ class CollectionImportHandler(logging.Handler):
         # This import cannot occur at import time because Django attempts to instantiate it early
         # which causes an unavoidable circular import as long as this needs to import any model
         from .models import CollectionImport
+        from pulpcore.plugin.models import Task
 
-        collection_import = CollectionImport.objects.get(task=get_current_job().id)
+        collection_import = CollectionImport.objects.get(task=Task.current().pulp_id)
         collection_import.add_log_record(record)
         collection_import.save()
