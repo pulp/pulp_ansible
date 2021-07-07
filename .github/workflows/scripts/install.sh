43f10fbb6f7d01843e8ea084c0742c61f052b7be
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-07-07 11:28:40 -0300

    Adding after failure to nightly + update CI
    [noissue]

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -80,6 +80,8 @@ fi
 
 cat >> vars/main.yaml << VARSYAML
 pulp_settings: {"allowed_export_paths": "/tmp", "allowed_import_paths": "/tmp", "ansible_api_hostname": "http://pulp:80", "ansible_content_hostname": "http://pulp:80/pulp/content"}
+pulp_scheme: http
+pulp_container_tag: latest
 VARSYAML
 
 if [ "$TEST" = "s3" ]; then
