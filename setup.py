f1b93133b2bfff58c46347020439770ffde1ef4e
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-07-21 19:53:55 -0300

    Update changelog
    [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst") as f:
 
 setup(
     name="pulp-ansible",
-    version="0.9.0.dev",
+    version="0.10.dev",
     description="Pulp plugin to manage Ansible content, e.g. roles",
     long_description=long_description,
     license="GPLv2+",
