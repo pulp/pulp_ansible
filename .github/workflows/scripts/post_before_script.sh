6b4531f961fa405ba80be02e0494d07d6d6f2148
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-07-22 12:28:53 -0300

    Increase max_connections
    https://www.ansible.com/hubfs/AnsibleFest%20ATL%20Slide%20Decks/Troubleshooting%20Tips%20and%20Tricks%20.pdf
    
    [noissue]

--- .github/workflows/scripts/post_before_script.sh
+++ .github/workflows/scripts/post_before_script.sh
@@ -6,3 +6,19 @@ password password
 " > ~/.netrc
 
 chmod og-rw ~/.netrc
+
+PG_CONF_PATH=$(docker exec -u postgres pulp psql -c 'SHOW config_file' | grep '\.conf' | xargs)
+docker exec -u postgres pulp sed -i 's/max_connections = 100/max_connections = 1024/g' $PG_CONF_PATH
+docker exec pulp bash -c "s6-svc -d /var/run/s6/services/pulpcore-api"
+sleep 5
+docker exec pulp bash -c "s6-svc -r /var/run/s6/services/postgresql"
+docker exec pulp bash -c "s6-svc -r /var/run/s6/services/pulpcore-content"
+docker exec pulp bash -c "s6-svc -r /var/run/s6/services/pulpcore-resource-manager"
+docker exec pulp bash -c "s6-svc -r /var/run/s6/services/pulpcore-worker@1"
+docker exec pulp bash -c "s6-svc -r /var/run/s6/services/pulpcore-worker@2"
+docker exec pulp bash -c "s6-svc -u /var/run/s6/services/pulpcore-api"
+docker exec pulp bash -c "s6-svc -r /var/run/s6/services/new-pulpcore-resource-manager"
+docker exec pulp bash -c "s6-svc -r /var/run/s6/services/new-pulpcore-worker@1"
+docker exec pulp bash -c "s6-svc -r /var/run/s6/services/new-pulpcore-worker@2"
+echo "Restarting postgres in 60 seconds"
+sleep 60
