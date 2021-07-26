d89ac223d319ff9470d1bc466490a904186c3007
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-07-26 16:30:49 -0300

    Update CI
    [noissue]

--- .github/workflows/scripts/post_docs_test.sh
+++ .github/workflows/scripts/post_docs_test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-export BASE_ADDR=http://pulp:80
-export CONTENT_ADDR=http://pulp:80
+export BASE_ADDR=https://pulp:443
+export CONTENT_ADDR=https://pulp:443
 
 cd docs/_scripts/
 bash ./quickstart.sh
