83262605ac67bf0cea8477b85d15f874173054a7
Dennis Kliban <dkliban@redhat.com>
2021-10-15 10:48:24 -0400

    Updates SyncHelpersMixin with GitRemote API client
    This also fixes _create_repo_and_sync_with_remote() in SyncHelpersMixin.
    
    [noissue]

--- pulp_ansible/tests/functional/api/git/test_crud_remotes.py
+++ pulp_ansible/tests/functional/api/git/test_crud_remotes.py
@@ -1,34 +1,29 @@
 # coding=utf-8
 """Tests that CRUD remotes."""
-from pulpcore.client.pulp_ansible import RemotesGitApi
+from pulp_smash.pulp3.bindings import monitor_task
 
-from pulp_smash.pulp3.bindings import monitor_task, PulpTestCase
-
-from pulp_ansible.tests.functional.utils import gen_ansible_client, gen_ansible_remote
+from pulp_ansible.tests.functional.utils import (
+    gen_ansible_remote,
+    TestCaseUsingBindings,
+)
 from pulp_ansible.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
 
 
-class CRUDAnsibleGitRemotesTestCase(PulpTestCase):
+class CRUDAnsibleGitRemotesTestCase(TestCaseUsingBindings):
     """CRUD Ansible Git remotes."""
-
-    @classmethod
-    def setUpClass(cls):
-        """Create class-wide variables."""
-        cls.client = gen_ansible_client()
-        cls.remote_ansible_git_api = RemotesGitApi(cls.client)
 
     def test_01_crud_remote(self):
         """Create, Read, Partial Update, and Delete an Ansible Git remote."""
         body = gen_ansible_remote(url="https://github.com/geerlingguy/ansible-role-adminer.git")
-        remote = self.remote_ansible_git_api.create(body)
-        self.addCleanup(self.remote_ansible_git_api.delete, remote.pulp_href)
-        remote = self.remote_ansible_git_api.read(remote.pulp_href)
+        remote = self.remote_git_api.create(body)
+        self.addCleanup(self.remote_git_api.delete, remote.pulp_href)
+        remote = self.remote_git_api.read(remote.pulp_href)
         for k, v in body.items():
             self.assertEquals(body[k], getattr(remote, k))
         update_body = {"url": "https://github.com/geerlingguy/ansible-role-ansible.git"}
-        remote_update = self.remote_ansible_git_api.partial_update(remote.pulp_href, update_body)
+        remote_update = self.remote_git_api.partial_update(remote.pulp_href, update_body)
         monitor_task(remote_update.task)
-        remote = self.remote_ansible_git_api.read(remote.pulp_href)
+        remote = self.remote_git_api.read(remote.pulp_href)
         self.assertEqual(remote.url, update_body["url"])
         self.assertEqual(remote.metadata_only, False)
         with self.assertRaises(AttributeError):
@@ -39,7 +34,7 @@ class CRUDAnsibleGitRemotesTestCase(PulpTestCase):
         body = gen_ansible_remote(
             url="https://github.com/geerlingguy/ansible-role-adminer.git", metadata_only=True
         )
-        remote = self.remote_ansible_git_api.create(body)
-        self.addCleanup(self.remote_ansible_git_api.delete, remote.pulp_href)
+        remote = self.remote_git_api.create(body)
+        self.addCleanup(self.remote_git_api.delete, remote.pulp_href)
         for k, v in body.items():
             self.assertEqual(body[k], getattr(remote, k))
