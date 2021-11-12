d518a4c9d43baa5fbe7c0055a36b883e7785e091
pulpbot <pulp-infra@redhat.com>
2021-11-12 19:27:06 +0000

    Release 0.7.4
    Redmine Query: https://pulp.plan.io/issues?set_filter=1&status_id=*&issue_id=9391,8857,8923
    Redmine Milestone: https://pulp.plan.io/versions/301.json
    [noissue]

--- docs/conf.py
+++ docs/conf.py
@@ -30,9 +30,9 @@ copyright = '2019, Pulp Ansible Team'
 author = 'Pulp Ansible Team'
 
 # The short X.Y version
-version = "0.7.4.dev"
+version = "0.7.4"
 # The full version, including alpha/beta/rc tags
-release = "0.7.4.dev"
+release = "0.7.4"
 
 
 # -- General configuration ---------------------------------------------------
