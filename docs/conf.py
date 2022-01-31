b1a70fd952aba45b842f833d1f4694341417e744
pulpbot <pulp-infra@redhat.com>
2022-01-31 15:22:38 +0000

    Release 0.7.5 GH Issues: 806,801
    [noissue]

--- docs/conf.py
+++ docs/conf.py
@@ -30,9 +30,9 @@ copyright = '2019, Pulp Ansible Team'
 author = 'Pulp Ansible Team'
 
 # The short X.Y version
-version = "0.7.5.dev"
+version = "0.7.5"
 # The full version, including alpha/beta/rc tags
-release = "0.7.5.dev"
+release = "0.7.5"
 
 
 # -- General configuration ---------------------------------------------------
