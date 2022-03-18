168385afca41eee8239506fe865f37c485f74dec
pulpbot <pulp-infra@redhat.com>
2022-03-18 14:21:33 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/publish_docs.sh
+++ .github/workflows/scripts/publish_docs.sh
@@ -38,6 +38,7 @@ if [[ "$GITHUB_WORKFLOW" == "Ansible changelog update" ]]; then
 fi
 
 # Building python bindings
+export PULP_URL="${PULP_URL:-https://pulp}"
 VERSION=$(http $PULP_URL/pulp/api/v3/status/ | jq --arg plugin ansible --arg legacy_plugin pulp_ansible -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')
 cd ../pulp-openapi-generator
 rm -rf pulp_ansible-client
