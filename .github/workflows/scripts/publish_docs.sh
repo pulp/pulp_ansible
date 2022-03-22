89540413b802af031dbec264ff59ac2026f95605
pulpbot <pulp-infra@redhat.com>
2022-03-22 13:37:50 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/publish_docs.sh
+++ .github/workflows/scripts/publish_docs.sh
@@ -37,19 +37,11 @@ if [[ "$GITHUB_WORKFLOW" == "Ansible changelog update" ]]; then
   exit
 fi
 
-# Building python bindings
-export PULP_URL="${PULP_URL:-https://pulp}"
-VERSION=$(http $PULP_URL/pulp/api/v3/status/ | jq --arg plugin ansible --arg legacy_plugin pulp_ansible -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')
-cd ../pulp-openapi-generator
-rm -rf pulp_ansible-client
-./generate.sh pulp_ansible python $VERSION
-cd pulp_ansible-client
+pip install mkdocs pymdown-extensions
 
-# Adding mkdocs
-find ./docs/* -exec sed -i 's/README//g' {} \;
-cp README.md docs/index.md
-sed -i 's/docs\///g' docs/index.md
-find ./docs/* -exec sed -i 's/\.md//g' {} \;
+mkdir -p ../bindings
+tar -xvf python-client-docs.tar --directory ../bindings
+cd ../bindings
 cat >> mkdocs.yml << DOCSYAML
 ---
 site_name: PulpAnsible Client
@@ -60,8 +52,6 @@ repo_name: pulp/pulp_ansible
 repo_url: https://github.com/pulp/pulp_ansible
 theme: readthedocs
 DOCSYAML
-
-pip install mkdocs pymdown-extensions
 
 # Building the bindings docs
 mkdocs build
