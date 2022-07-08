7086614bd37cf6e53b5efb0cc829b01511dcdace
James Tanner <tanner.jc@gmail.com>
2022-07-07 20:43:31 -0400

    Switch to function based test along with fixtures.
    [noissue]

--- pulp_ansible/tests/functional/cli/test_role_import.py
+++ pulp_ansible/tests/functional/cli/test_role_import.py
@@ -1,5 +1,7 @@
 """Tests that Roles hosted by Pulp can be installed by ansible-galaxy."""
 
+import json
+import pytest
 import subprocess
 
 from pulp_smash.pulp3.bindings import monitor_task
@@ -13,85 +15,72 @@ from pulp_ansible.tests.functional.constants import (
 from pulp_ansible.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
 
 
-class ImportRoleTestCase(TestCaseUsingBindings):
-    """Test whether ansible-galaxy can import a role into pulp."""
+# @pytest.mark.parallel
+def test_role_import(
+    delete_orphans_pre,
+    ansible_bindings_client,
+    ansible_galaxy_roles_api_client,
+    ansible_repo_api_client,
+    ansible_distributions_api_client,
+    gen_object_with_cleanup
+):
+    """
+    Verifies the api/v1/imports view is functional.
+    """
+    # Create the legacy repo if needed
+    repo = gen_object_with_cleanup(ansible_repo_api_client, {"name": "legacy"})
 
-    def test_import_role(self):
-        """
-        Verifies the api/v1/imports view is functional.
-        """
-        # Create the legacy repo if needed
-        legacy_repo_check = self.repo_api.list(name="legacy")
-        if legacy_repo_check.count == 1:
-            repo = legacy_repo_check.results[0]
-        else:
-            repo = self.repo_api.create({"name": "legacy"})
-        self.addCleanup(self.repo_api.delete, repo.pulp_href)
+    # Create the legacy distribution if needed
+    body = {
+        "name": "legacy",
+        "base_path": "legacy",
+    }
+    body["repository"] = repo.pulp_href
+    distribution = gen_object_with_cleanup(ansible_distributions_api_client, body)
 
-        # Create the legacy distribution if needed
-        legacy_dist_check = self.distributions_api.list(name="legacy")
-        if legacy_dist_check.count == 1:
-            distribution = legacy_dist_check.results[0]
-        else:
-            body = {
-                "name": "legacy",
-                "base_path": "legacy",
-            }
-            body["repository"] = repo.pulp_href
-            distribution_create = self.distributions_api.create(body)
-            created_resources = monitor_task(distribution_create.task).created_resources
-            distribution = self.distributions_api.read(created_resources[0])
-        self.addCleanup(self.distributions_api.delete, distribution.pulp_href)
+    # We expect the http[s]://<host>:<port>/api/v1 endpoint to be available,
+    # so no special distribution based url will be used. The import view
+    # should take care of defaulting to the "legacy" distribution.
+    client = ansible_repo_api_client.api_client
+    server_url = client.configuration.host.rstrip("/") + "/"
 
-        # We expect the http[s]://<host>:<port>/api/v1 endpoint to be available,
-        # so no special distribution based url will be used. The import view
-        # should take care of defaulting to the "legacy" distribution.
-        server_url = self.client.configuration.host.rstrip("/") + "/"
+    # Ensure the galaxy cli can import the role without error ...
+    cmd = [
+        "ansible-galaxy",
+        "role",
+        "import",
+        "-vvvv",
+        ANSIBLE_DOCKER_ROLE_NAMESPACE,
+        ANSIBLE_DOCKER_ROLE_REPO,
+        "-c",
+        "-s",
+        server_url,
+    ]
+    pid = subprocess.run(cmd)
+    assert pid.returncode == 0
 
-        # Ensure the galaxy cli can import the role without error ...
-        cmd = [
-            "ansible-galaxy",
-            "role",
-            "import",
-            "-vvvv",
-            ANSIBLE_DOCKER_ROLE_NAMESPACE,
-            ANSIBLE_DOCKER_ROLE_REPO,
-            "-c",
-            "-s",
-            server_url,
-        ]
-        pid = subprocess.run(cmd)
-        self.assertEqual(pid.returncode, 0)
+    # Fetch the first page of the v1 roles.
+    v1_roles_p1 = ansible_galaxy_roles_api_client.get()
 
-        """ content roles api has no delete function?
-        # get the content view's pulp_href for cleanup?
-        c_roles = self.content_roles_api.list()
-        for c_role in c_roles.results:
-            if (
-                c_role.namespace == ANSIBLE_DOCKER_ROLE_NAMESPACE
-                and c_role.name == ANSIBLE_DOCKER_ROLE_NAME
-            ):
-                self.addCleanup(self.content_roles_api.delete, c_role.pulp_href)
-                break
-        """
+    # Ensure the imported role is in the list.
+    expected_role = {
+        "id": f"{ANSIBLE_DOCKER_ROLE_NAMESPACE}.{ANSIBLE_DOCKER_ROLE_NAME}",
+        "name": ANSIBLE_DOCKER_ROLE_NAME,
+        "namespace": ANSIBLE_DOCKER_ROLE_NAMESPACE,
+    }
+    assert expected_role in [x.to_dict() for x in v1_roles_p1.results]
 
-        # Fetch the first page of the v1 roles.
-        v1_roles_p1 = self.galaxy_roles_api.get()
-
-        # Ensure the imported role is in the list.
-        expected_role = {
-            "id": f"{ANSIBLE_DOCKER_ROLE_NAMESPACE}.{ANSIBLE_DOCKER_ROLE_NAME}",
-            "name": ANSIBLE_DOCKER_ROLE_NAME,
-            "namespace": ANSIBLE_DOCKER_ROLE_NAMESPACE,
-        }
-        self.assertIn(expected_role, [x.to_dict() for x in v1_roles_p1.results])
-
-        # Ensure it has a list of versions ...
-        v_url = (
-            server_url
-            + "api/v1/roles/"
-            + f"{ANSIBLE_DOCKER_ROLE_NAMESPACE}.{ANSIBLE_DOCKER_ROLE_NAME}"
-            + "/versions/"
-        )
-        vpage1 = self.api_client.get(v_url)
-        self.assertGreaterEqual(vpage1["count"], 1)
+    # Ensure it has a list of versions ...
+    v_url = (
+        "/api/v1/roles/"
+        + f"{ANSIBLE_DOCKER_ROLE_NAMESPACE}.{ANSIBLE_DOCKER_ROLE_NAME}"
+        + "/versions/"
+    )
+    # vpage1 = self.api_client.get(v_url)
+    # import epdb; epdb.st()
+    # vpage1 = client.get(v_url)
+    # vpage1 = client.call_api(v_url, 'GET')
+    resp = ansible_bindings_client.rest_client.GET(server_url.rstrip('/') + v_url)
+    vpage1 = json.loads(resp.data)
+    # import epdb; epdb.st()
+    assert vpage1["count"] >= 1
