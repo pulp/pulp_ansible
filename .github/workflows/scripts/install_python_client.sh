fab72e8cf6f10926c7e12b04cd1caf100403c8c9
pulpbot <pulp-infra@redhat.com>
2022-07-10 13:37:39 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/install_python_client.sh
+++ .github/workflows/scripts/install_python_client.sh
@@ -7,7 +7,7 @@
 #
 # For more info visit https://github.com/pulp/plugin_template
 
-set -euv
+set -mveuo pipefail
 
 export PULP_URL="${PULP_URL:-https://pulp}"
 
@@ -30,7 +30,7 @@ export response=$(curl --write-out %{http_code} --silent --output /dev/null http
 if [ "$response" == "200" ];
 then
   echo "pulp_ansible client $VERSION has already been released. Installing from PyPI."
-  pip install pulp-ansible-client==$VERSION
+  docker exec pulp pip3 install pulp-ansible-client==$VERSION
   mkdir -p dist
   tar cvf python-client.tar ./dist
   exit
@@ -41,7 +41,7 @@ rm -rf pulp_ansible-client
 ./generate.sh pulp_ansible python $VERSION
 cd pulp_ansible-client
 python setup.py sdist bdist_wheel --python-tag py3
-find . -name "*.whl" -exec pip install {} \;
+find . -name "*.whl" -exec docker exec pulp pip3 install /root/pulp-openapi-generator/pulp_ansible-client/{} \;
 tar cvf ../../pulp_ansible/python-client.tar ./dist
 
 find ./docs/* -exec sed -i 's/Back to README/Back to HOME/g' {} \;
