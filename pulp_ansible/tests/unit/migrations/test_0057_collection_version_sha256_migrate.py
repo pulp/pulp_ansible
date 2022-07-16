609b2e3530078a19bda6c73d45481ea7a942c983
Gerrod Ubben <gerrodubben@gmail.com>
2022-07-15 21:13:19 -0400

    Add sha256 uniqueness to CollectionVersion
    This should be ZDU compatible. The transition will be completed with the
    next y-release.
    
    fixes: #1052
    
    Co-authored-by: Matthias Dellweg <mdellweg@redhat.com>

---
+++ pulp_ansible/tests/unit/migrations/test_0057_collection_version_sha256_migrate.py
@@ -0,0 +1,39 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+
+def test_collection_version_sha256_migrate(migrate):
+    apps = migrate([("ansible", "0055_alter_collectionversion_version_alter_role_version")])
+    Artifact = apps.get_model("core", "Artifact")
+    Collection = apps.get_model("ansible", "Collection")
+    CollectionVersion = apps.get_model("ansible", "CollectionVersion")
+
+    artifact = Artifact.objects.create(
+        size=8, sha256="SENTINEL", file=SimpleUploadedFile("foo", b"deadbeef")
+    )
+    collection = Collection.objects.create(
+        namespace="snap",
+        name="crackle",
+    )
+    cv = CollectionVersion.objects.create(
+        pulp_type="collection_version",
+        collection=collection,
+        namespace="snap",
+        name="crackle",
+        version="pop",
+        version_minor="1",
+        version_major="2",
+        version_patch="3",
+    )
+    cv.contentartifact_set.create(artifact=artifact)
+
+    apps = migrate([("ansible", "0056_collectionversion_sha256")])
+    CollectionVersion = apps.get_model("ansible", "CollectionVersion")
+
+    cv = CollectionVersion.objects.get(pk=cv.pk)
+    assert cv.sha256 == ""
+
+    apps = migrate([("ansible", "0057_collectionversion_sha256_migrate")])
+    CollectionVersion = apps.get_model("ansible", "CollectionVersion")
+
+    cv = CollectionVersion.objects.get(pk=cv.pk)
+    assert cv.sha256 == "SENTINEL"
