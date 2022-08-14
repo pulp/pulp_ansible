8a739094e3e655a08a4a7b362d2b7a82a7209d8e
pulpbot <pulp-infra@redhat.com>
2022-08-14 04:05:59 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/before_install.sh
+++ .github/workflows/scripts/before_install.sh
@@ -104,7 +104,7 @@ if [ -n "$PULP_CLI_PR_NUMBER" ]; then
 fi
 
 cd pulp-cli
-pip install -e .
+pip install .
 pulp config create --base-url https://pulp --location tests/cli.toml 
 mkdir ~/.config/pulp
 cp tests/cli.toml ~/.config/pulp/cli.toml
