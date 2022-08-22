40283fa66ee4bfcdb5a007ba2c0ce512d2d6fccb
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-08-22 15:43:35 +0100

    Fix tar name
    [noissue]

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -29,7 +29,6 @@ PULPCORE=./pulpcore
 if [[ "$TEST" == "plugin-from-pypi" ]]; then
   PLUGIN_NAME=pulp_ansible
 elif [[ "${RELEASE_WORKFLOW:-false}" == "true" ]]; then
-  ls -al ./pulp_ansible/dist/
   PLUGIN_NAME=./pulp_ansible/dist/pulp-ansible-$PLUGIN_VERSION.tar.gz
 else
   PLUGIN_NAME=./pulp_ansible
