90350ff6147c70ee266630fe293e149f4f6d5e18
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-08-22 23:56:35 +0100

    Fix pulpcore doc
    [noissue]

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -126,7 +126,11 @@ ansible-playbook build_container.yaml
 ansible-playbook start_container.yaml
 
 if [[ "$TEST" = "docs" || "$TEST" = "publish" ]]; then
-  cmd_prefix bash -c "cd pulpcore; pip install -r doc_requirements.txt"
+  if [[ "${RELEASE_WORKFLOW:-false}" == "true" ]]; then
+    cmd_prefix bash -c "pip install -r https://raw.githubusercontent.com/pulp/pulpcore/3.19/doc_requirements.txt"
+  else
+    cmd_prefix bash -c "cd pulpcore; pip install -r doc_requirements.txt"
+  fi
   cmd_prefix bash -c "cd pulp_ansible; pip install -r doc_requirements.txt"
 fi
 echo ::group::SSL
