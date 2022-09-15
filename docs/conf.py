212919990decd9d2f464fe6ba9d75ef993bf8348
pulpbot <pulp-infra@redhat.com>
2022-09-15 10:43:13 +0000

    Release 0.14.2 GH Issues: 1202
    [noissue]

--- docs/conf.py
+++ docs/conf.py
@@ -30,9 +30,9 @@ copyright = '2019, Pulp Ansible Team'
 author = 'Pulp Ansible Team'
 
 # The short X.Y version
-version = "0.14.2.dev"
+version = "0.14.2"
 # The full version, including alpha/beta/rc tags
-release = "0.14.2.dev"
+release = "0.14.2"
 
 
 # -- General configuration ---------------------------------------------------
--- pulp_ansible/app/__init__.py
+++ pulp_ansible/app/__init__.py
@@ -8,5 +8,5 @@ class PulpAnsiblePluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_ansible.app"
     label = "ansible"
-    version = "0.14.2.dev"
+    version = "0.14.2"
     python_package_name = "pulp-ansible"
