f87caacd7b29d20748c91b6277f6e5cf4c14185a
pulpbot <pulp-infra@redhat.com>
2022-09-15 10:43:13 +0000

    Bump to 0.14.3.dev
    [noissue]

--- docs/conf.py
+++ docs/conf.py
@@ -30,9 +30,9 @@ copyright = '2019, Pulp Ansible Team'
 author = 'Pulp Ansible Team'
 
 # The short X.Y version
-version = "0.14.2"
+version = "0.14.3.dev"
 # The full version, including alpha/beta/rc tags
-release = "0.14.2"
+release = "0.14.3.dev"
 
 
 # -- General configuration ---------------------------------------------------
