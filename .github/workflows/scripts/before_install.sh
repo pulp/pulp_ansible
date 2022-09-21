c9851299190e1914ca4cadef44f3c9a4d4549474
Ina Panova <ipanova@redhat.com>
2022-09-21 19:52:51 +0200

    Update CI before the release
    [noissue]

--- .github/workflows/scripts/before_install.sh
+++ .github/workflows/scripts/before_install.sh
@@ -111,7 +111,7 @@ cp tests/cli.toml ~/.config/pulp/cli.toml
 cd ..
 
 
-git clone --depth=1 https://github.com/pulp/pulpcore.git --branch main
+git clone --depth=1 https://github.com/pulp/pulpcore.git --branch 3.21
 
 cd pulpcore
 
