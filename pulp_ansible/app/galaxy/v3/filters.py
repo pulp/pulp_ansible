fe63f26141cd7b9794f061b575d1e0af15bd90e0
James Tanner <tanner.jc@gmail.com>
2023-01-10 16:56:56 -0500

    Commit.
    [noissue]
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- pulp_ansible/app/galaxy/v3/filters.py
+++ pulp_ansible/app/galaxy/v3/filters.py
@@ -14,74 +14,54 @@ from pulp_ansible.app.viewsets import (
 class CollectionVersionSearchFilter(FilterSet):
 
     name = filters.CharFilter(
-        field_name="name",
         method="filter_by_name",
     )
 
     namespace = filters.CharFilter(
-        field_name="namepace",
         method="filter_by_namespace",
     )
 
     distribution = filters.CharFilter(
-        field_name="repository",
         method="filter_by_repository_name",
     )
 
     repository = filters.CharFilter(
-        field_name="repository",
         method="filter_by_repository_name",
     )
 
     repository_name = filters.CharFilter(
-        field_name="repository",
         method="filter_by_repository_name",
     )
 
-    dependency = filters.CharFilter(field_name="collection_version", method="filter_by_dependency")
+    dependency = filters.CharFilter(method="filter_by_dependency")
 
-    deprecated = filters.CharFilter(field_name="deprecated", method="filter_by_deprecated")
+    deprecated = filters.CharFilter(method="filter_by_deprecated")
 
     def filter_by_repository_name(self, qs, name, value):
         """Allow for multiple repository names to filter on."""
         include_q = Q()
-        repository_names = value.split(",")
-        for rn in repository_names:
-            include_q = include_q | Q(repository__name=rn)
+        if ',' in value:
+            repository_names = value.split(",")
+            for rn in repository_names:
+                include_q = include_q | Q(repository__name=rn)
+        else:
+            include_q = include_q | Q(repository__name=value)
         qs = qs.filter(include_q)
         return qs
 
     def filter_by_name(self, qs, name, value):
         print('FILTER BY NAME')
-        return qs.filter(Q(content__collection_version__name=name))
+        return qs.filter(Q(content__ansible_collectionversion__name=value))
+        return qs
 
     def filter_by_namespace(self, qs, name, value):
         print('FILTER BY NAMESPACE')
-        return qs.filter(Q(content__collection_version__namespace=name))
+        return qs.filter(Q(content__ansible_collectionversion__namespace=value))
 
     def filter_by_dependency(self, qs, name, value):
         """Return a list of collections that depend on a given collection name."""
-
-        #kwargs = {f"dependencies__{value}__isnull": False}
-        #qs = qs.filter(**kwargs)
-
-        obj = qs.first()
-        print(f'{datetime.datetime.now().isoformat()} OBJ: {type(obj)}')
-
-        '''
-        for x in dir(obj):
-            print(f'OBJ: .{x}')
-
-        print(f'OBJ.content: {obj.content}')
-        for x in dir(obj.content):
-            print(f'OBJ.content: .{x}')
-        '''
-
-        return qs
-
-        kwargs = {f"collection_version__dependencies__{value}__isnull": False}
+        kwargs = {f"content__ansible_collectionversion__dependencies__{value}__isnull": False}
         qs = qs.filter(**kwargs)
-
         return qs
 
     def filter_by_deprecated(self, qs, name, value):
