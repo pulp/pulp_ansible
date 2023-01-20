995d8a385cb5ea58218370e4955c66604c2614f2
Matthias Dellweg <mdellweg@redhat.com>
2023-01-20 11:24:40 +0100

    Fix line breaks in cli examples (#1329)
    (cherry picked from commit f9e549a3a58df6454ba0f1d389949ab3da4535f5)

--- docs/_scripts/remote-collection.sh
+++ docs/_scripts/remote-collection.sh
@@ -2,6 +2,6 @@
 pulp ansible remote -t "collection" create \
     --name "cbar" \
     --url "https://galaxy-dev.ansible.com/" \
-    --requirements "collections:\n  - testing.ansible_testing_content"
-# If requirements are in a file instead
-# you can use the option --requirements-file <file_name>
+    --requirements $'collections:\n  - testing.ansible_testing_content'
+# If requirements are in a file
+# you can use the option '--requirements @<file_name>' instead.
