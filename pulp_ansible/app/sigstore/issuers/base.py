144b352ad250099a67ae8e8414b4db625ad0367c
Maya Costantini <mcostant@redhat.com>
2023-05-23 13:27:25 +0000

    Remove unused imports from sigsore OIDC issuers base.py

--- pulp_ansible/app/sigstore/issuers/base.py
+++ pulp_ansible/app/sigstore/issuers/base.py
@@ -2,18 +2,6 @@
 Base class for representing Sigstore OIDC issuers.
 """
 
-from sigstore.oidc import _OpenIDConfiguration
-from sigstore.oidc import IdentityError
-
-import logging
-import requests
-import time
-import urllib.parse
-import webbrowser
-
-log = logging.getLogger(__name__)
-
-
 class BaseIssuer:
     """Representation of a base OIDC issuer."""
     def __init__(self, *args, **kwargs):
