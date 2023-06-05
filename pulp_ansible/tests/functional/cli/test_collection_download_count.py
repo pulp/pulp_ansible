522b943c89f0e6f3de731c495d41db7981383bc0
Brian McLaughlin <bmclaugh@redhat.com>
2023-06-05 14:45:04 -0400

    Update monitor_task and pulp_smash usage [noissue]

--- pulp_ansible/tests/functional/cli/test_collection_download_count.py
+++ pulp_ansible/tests/functional/cli/test_collection_download_count.py
@@ -2,11 +2,7 @@
 from os import path
 import subprocess
 import pytest
-from urllib.parse import urljoin
-
 from pulpcore.client.pulp_ansible import AnsibleRepositorySyncURL
-from pulp_smash import api, config
-from pulp_smash.pulp3.bindings import monitor_task
 
 from pulp_ansible.tests.functional.constants import (
     ANSIBLE_DEMO_COLLECTION,
@@ -23,6 +19,7 @@ def install_scenario_distribution(
     ansible_repo_factory,
     ansible_collection_remote_factory,
     ansible_distribution_factory,
+    monitor_task,
 ):
     """Prepare a distribution to install from."""
     collection_requirements = f"collections:\n  - {ANSIBLE_DEMO_COLLECTION}"
@@ -40,48 +37,29 @@ def install_scenario_distribution(
     return ansible_distribution_factory(repo)
 
 
-@pytest.fixture(scope="session")
-def pulp_client():
-    """Create and configure a Pulp API client, including custom authentication headers."""
-    cfg = config.get_config()
-    client = api.Client(cfg)
-    headers = cfg.custom.get("headers", None)
-    if headers:
-        client.request_kwargs.setdefault("headers", {}).update(headers)
-    return client
-
-
-def get_galaxy_url(base, path):
-    """Given an Ansible Distribution base_path and an endpoint path, construct a URL.
-
-    Takes the expected GALAXY_API_ROOT setting of the target into consideration.
-    """
-    cfg = config.get_config()
-    path = path.lstrip("/")
-    GALAXY_API_ROOT = cfg.custom.get(
-        "galaxy_api_root", "/pulp_ansible/galaxy/%(base_path)s/api/"
-    ) % {"base_path": base}
-    return urljoin(GALAXY_API_ROOT, path)
-
-
 def test_collection_download_count(
+    ansible_dir_factory,
+    galaxy_v3_collections_api_client,
     install_scenario_distribution,
     pulp_admin_user,
-    ansible_dir_factory,
-    pulp_client,
 ):
     """Test that the collection download counting functions."""
     with pulp_admin_user:
         collection_namespace = ANSIBLE_DEMO_COLLECTION.split(".")[0]
         collection_name = ANSIBLE_DEMO_COLLECTION.split(".")[1]
-        url = get_galaxy_url(
-            install_scenario_distribution.base_path,
-            f"/v3/collections/{collection_namespace}/{collection_name}/",
+        collection_detail = galaxy_v3_collections_api_client.read(
+            path=install_scenario_distribution.base_path,
+            namespace=collection_namespace,
+            name=collection_name,
         )
-        collection_detail = pulp_client.using_handler(api.json_handler).get(url)
 
+        collection_detail = galaxy_v3_collections_api_client.read(
+            path=install_scenario_distribution.base_path,
+            namespace=collection_namespace,
+            name=collection_name,
+        )
         # Collection is installed twice in cli.test_collection_install
-        assert collection_detail["download_count"] == 2
+        assert collection_detail.download_count == "2"
 
         temp_dir = str(
             ansible_dir_factory(install_scenario_distribution.client_url, pulp_admin_user)
@@ -105,8 +83,12 @@ def test_collection_download_count(
         subprocess.run(cmd, cwd=temp_dir)
         assert path.exists(directory), "Could not find directory {}".format(directory)
 
-        collection_detail = pulp_client.using_handler(api.json_handler).get(url)
-        assert collection_detail["download_count"] == 3
+        collection_detail = galaxy_v3_collections_api_client.read(
+            path=install_scenario_distribution.base_path,
+            namespace=collection_namespace,
+            name=collection_name,
+        )
+        assert collection_detail.download_count == "3"
 
         cmd = [
             "ansible-galaxy",
@@ -121,5 +103,9 @@ def test_collection_download_count(
 
         subprocess.run(cmd, cwd=temp_dir)
 
-        collection_detail = pulp_client.using_handler(api.json_handler).get(url)
-        assert collection_detail["download_count"] == 4
+        collection_detail = galaxy_v3_collections_api_client.read(
+            path=install_scenario_distribution.base_path,
+            namespace=collection_namespace,
+            name=collection_name,
+        )
+        assert collection_detail.download_count == "4"
