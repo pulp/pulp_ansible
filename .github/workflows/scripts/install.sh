af83d406564fdfed9bd7f33aa81b4c9e83ffbf8b
pulpbot <pulpbot@users.noreply.github.com>
2023-08-28 04:20:57 -0400

    Update CI files (#1564)
    [noissue]

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -65,13 +65,14 @@ services:
       - ../../../pulp-openapi-generator:/root/pulp-openapi-generator
     env:
       PULP_WORKERS: "4"
+      PULP_HTTPS: "true"
 VARSYAML
 
 cat >> vars/main.yaml << VARSYAML
 pulp_settings: {"allowed_export_paths": "/tmp", "allowed_import_paths": "/tmp", "ansible_api_hostname": "https://pulp:443", "ansible_content_hostname": "https://pulp:443/pulp/content"}
 pulp_scheme: https
 
-pulp_container_tag: https
+pulp_container_tag: "latest"
 
 VARSYAML
 
