658199a16a5af730a9bb74b2d6eb70f9ecd5231b
pulpbot <pulp-infra@redhat.com>
2023-10-03 15:00:35 +0000

    Release 0.20.1 GH Issues: 1598
    [noissue]

--- pulp_ansible/app/__init__.py
+++ pulp_ansible/app/__init__.py
@@ -8,5 +8,5 @@ class PulpAnsiblePluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_ansible.app"
     label = "ansible"
-    version = "0.20.1.dev"
+    version = "0.20.1"
     python_package_name = "pulp-ansible"
