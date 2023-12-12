40f131562b8b080939c3c1bd6251ba892e9a1ea8
Matthias Dellweg <mdellweg@redhat.com>
2023-12-12 15:22:29 +0100

    Fix a uniqueness violation for is_highest
    When syncing content, we need to ignore the upstream status of is_highest.
    
    [noissue]
    
    (cherry picked from commit 8d7550b3afedf4eb62abdff8b03a197605320392)

--- pulp_ansible/app/tasks/collections.py
+++ pulp_ansible/app/tasks/collections.py
@@ -1215,5 +1215,6 @@ class AnsibleContentSaver(ContentSaver):
                         continue
                     setattr(collection_version, attr_name, attr_value)
 
+                collection_version.is_highest = False
                 collection_version.save()
                 _update_highest_version(collection_version)
