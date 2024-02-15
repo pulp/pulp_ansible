688b38fd12c6aa6b61e3c7a771806e39a373df14
pulpbot <pulp-infra@redhat.com>
2024-02-15 15:55:22 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -82,9 +82,7 @@ cat >> vars/main.yaml << VARSYAML
 pulp_env: {}
 pulp_settings: {"allowed_export_paths": "/tmp", "allowed_import_paths": "/tmp", "ansible_api_hostname": "https://pulp:443", "ansible_collect_download_log": true, "ansible_content_hostname": "https://pulp:443/pulp/content", "ansible_signature_require_verification": false}
 pulp_scheme: https
-
-pulp_container_tag: "latest"
-
+pulp_default_container: ghcr.io/pulp/pulp-ci-centos:latest
 VARSYAML
 
 if [ "$TEST" = "s3" ]; then
