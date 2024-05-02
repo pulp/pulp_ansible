321241637fa74bb8563804ffff5cf1ba8be3051c
Matthias Dellweg <mdellweg@redhat.com>
2024-05-02 12:31:39 +0200

    Rewrite migration test to pytest
    [noissue]

---
+++ pulp_ansible/tests/unit/migrations/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+from django.db import connection
+from django.db.migrations.executor import MigrationExecutor
+
+
+@pytest.fixture
+def migrate(django_db_serialized_rollback):
+    executor = MigrationExecutor(connection)
+
+    def _migrate(target):
+        state = executor.migrate(target)
+        executor.loader.build_graph()
+        return state.apps
+
+    return _migrate
--- pulp_ansible/tests/unit/migrations/test_x_repo_search_migration.py
+++ pulp_ansible/tests/unit/migrations/test_x_repo_search_migration.py
@@ -1,63 +1,14 @@
-#!/usr/bin/env python
+def test_cross_repo_search_migration(migrate):
+    apps = migrate([("ansible", "0050_crossrepositorycollectionversionindex")])
+    AnsibleRepository = apps.get_model("ansible", "AnsibleRepository")
+    AnsibleDistribution = apps.get_model("ansible", "AnsibleDistribution")
 
-from django.db import connection
-from django.test import TestCase
-from django.db.migrations.executor import MigrationExecutor
+    # make a repository
+    repository = AnsibleRepository.objects.create(pulp_type="ansible", name="foobar")
+    repository.versions.create(number=0)
 
-
-class MigrationTestCase(TestCase):
-
-    @property
-    def app(self):
-        return "ansible"
-
-    migrate_from = None
-    migrate_to = None
-
-    def setUp(self):
-        assert (
-            self.migrate_from and self.migrate_to
-        ), "TestCase '{}' must define migrate_from and migrate_to properties".format(
-            type(self).__name__
-        )
-        self.migrate_from = [(self.app, self.migrate_from)]
-        self.migrate_to = [(self.app, self.migrate_to)]
-        executor = MigrationExecutor(connection)
-        old_apps = executor.loader.project_state(self.migrate_from).apps
-
-        # Reverse to the original migration
-        executor.migrate(self.migrate_from)
-
-        self.setUpBeforeMigration(old_apps)
-
-        # Run the migration to test
-        executor = MigrationExecutor(connection)
-        executor.loader.build_graph()  # reload.
-        executor.migrate(self.migrate_to)
-
-        self.apps = executor.loader.project_state(self.migrate_to).apps
-
-    def setUpBeforeMigration(self, apps):
-        pass
-
-
-class TestCrossRepoSearchMigration(MigrationTestCase):
-
-    migrate_from = "0050_crossrepositorycollectionversionindex"
-    migrate_to = "0051_cvindex_build"
-
-    def setUpBeforeMigration(self, apps):
-        AnsibleRepository = apps.get_model("ansible", "AnsibleRepository")
-        AnsibleDistribution = apps.get_model("ansible", "AnsibleDistribution")
-
-        # make a repository
-        repository = AnsibleRepository.objects.create(pulp_type="ansible", name="foobar")
-        repository.versions.create(number=0)
-
-        # make a distro
-        distro = AnsibleDistribution.objects.create(
-            pulp_type="ansible", name="foobar", base_path="foobar", repository=repository
-        )
-
-    def test_nothing(self):
-        pass
+    # make a distro
+    distro = AnsibleDistribution.objects.create(
+        pulp_type="ansible", name="foobar", base_path="foobar", repository=repository
+    )
+    migrate([("ansible", "0051_cvindex_build")])
