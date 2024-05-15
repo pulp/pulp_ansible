ad6ea67ce753f4a2a57f5cf4ac440ead876d7d06
pulpbot <pulp-infra@redhat.com>
2024-05-15 09:56:29 +0000

    Bump version: 0.20.6 → 0.20.7.dev

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst") as f:
 
 setup(
     name="pulp-ansible",
-    version="0.20.6",
+    version="0.20.7.dev",
     description="Pulp plugin to manage Ansible content, e.g. roles",
     long_description=long_description,
     license="GPLv2+",
