pulp/pulpcore

Update CI files
[noissue]

.github/workflows/scripts/build_python_client.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script expects all <app_label>-api.json files to exist in the plugins root directory.
+# It produces a <app_label>-python-client.tar and <app_label>-python-client-docs.tar file in the plugins root directory.
+
 # WARNING: DO NOT EDIT!
 #
 # This file was generated by plugin_template, and is managed by it. Please use
@@ -12,36 +15,20 @@ set -mveuo pipefail
 # make sure this script runs at the repo root
 cd "$(dirname "$(realpath -e "$0")")"/../../..
 
-source .github/workflows/scripts/utils.sh
-
-PULP_URL="${PULP_URL:-https://pulp}"
-export PULP_URL
-PULP_API_ROOT="${PULP_API_ROOT:-/pulp/}"
-export PULP_API_ROOT
-
-REPORTED_STATUS="$(pulp status)"
-REPORTED_VERSION="$(echo "$REPORTED_STATUS" | jq --arg plugin "core" -r '.versions[] | select(.component == $plugin) | .version')"
-VERSION="$(echo "$REPORTED_VERSION" | python -c 'from packaging.version import Version; print(Version(input()))')"
-
 pushd ../pulp-openapi-generator
-rm -rf pulpcore-client
+rm -rf "pulpcore-client"
 
-if pulp debug has-plugin --name "core" --specifier ">=3.44.0.dev"
-then
-  curl --fail-with-body -k -o api.json "${PULP_URL}${PULP_API_ROOT}api/v3/docs/api.json?bindings&component=core"
-  USE_LOCAL_API_JSON=1 ./generate.sh pulpcore python "$VERSION"
-else
-  ./generate.sh pulpcore python "$VERSION"
-fi
+# We need to copy that over to be visible in the container.
+cp "../pulpcore/core-api.json" .
+./gen-client.sh "core-api.json" "core" python "pulpcore"
 
 pushd pulpcore-client
 python setup.py sdist bdist_wheel --python-tag py3
 
-twine check "dist/pulpcore_client-$VERSION-py3-none-any.whl"
-twine check "dist/pulpcore-client-$VERSION.tar.gz"
+twine check "dist/pulpcore_client-"*"-py3-none-any.whl"
+twine check "dist/pulpcore-client-"*".tar.gz"
 
-cmd_prefix pip3 install "/root/pulp-openapi-generator/pulpcore-client/dist/pulpcore_client-${VERSION}-py3-none-any.whl"
-tar cvf ../../pulpcore/core-python-client.tar ./dist
+tar cvf "../../pulpcore/core-python-client.tar" ./dist
 
 find ./docs/* -exec sed -i 's/Back to README/Back to HOME/g' {} \;
 find ./docs/* -exec sed -i 's/README//g' {} \;

.github/workflows/scripts/build_ruby_client.sh
@@ -1,0 +1,35 @@
+#!/bin/bash
+
+# This script expects all <app_label>-api.json files to exist in the plugins root directory.
+# It produces a <app_label>-ruby-client.tar file in the plugins root directory.
+
+# WARNING: DO NOT EDIT!
+#
+# This file was generated by plugin_template, and is managed by it. Please use
+# './plugin-template --github pulpcore' to update this file.
+#
+# For more info visit https://github.com/pulp/plugin_template
+
+set -mveuo pipefail
+
+# make sure this script runs at the repo root
+cd "$(dirname "$(realpath -e "$0")")"/../../..
+
+pushd ../pulp-openapi-generator
+rm -rf "pulpcore-client"
+
+# We need to copy that over to be visible in the container.
+#cp "../pulpcore/core-api.json" .
+#./gen-client.sh "core-api.json" "core" ruby "pulpcore"
+
+# -------------
+# The generator still needs to have it called api.json at this time...
+cp "../pulpcore/api.json" .
+./gen-client.sh "api.json" "core" ruby "pulpcore"
+# -------------
+
+pushd pulpcore-client
+gem build pulpcore_client
+tar cvf "../../pulpcore/core-ruby-client.tar" "./pulpcore_client-"*".gem"
+popd
+popd

pulp/pulp_ansible

.github/workflows/scripts/build_python_client.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script expects all <app_label>-api.json files to exist in the plugins root directory.
+# It produces a <app_label>-python-client.tar and <app_label>-python-client-docs.tar file in the plugins root directory.
+
 # WARNING: DO NOT EDIT!
 #
 # This file was generated by plugin_template, and is managed by it. Please use
@@ -12,36 +15,20 @@ set -mveuo pipefail
 # make sure this script runs at the repo root
 cd "$(dirname "$(realpath -e "$0")")"/../../..
 
-source .github/workflows/scripts/utils.sh
-
-PULP_URL="${PULP_URL:-https://pulp}"
-export PULP_URL
-PULP_API_ROOT="${PULP_API_ROOT:-/pulp/}"
-export PULP_API_ROOT
-
-REPORTED_STATUS="$(pulp status)"
-REPORTED_VERSION="$(echo "$REPORTED_STATUS" | jq --arg plugin "ansible" -r '.versions[] | select(.component == $plugin) | .version')"
-VERSION="$(echo "$REPORTED_VERSION" | python -c 'from packaging.version import Version; print(Version(input()))')"
-
 pushd ../pulp-openapi-generator
-rm -rf pulp_ansible-client
+rm -rf "pulp_ansible-client"
 
-if pulp debug has-plugin --name "core" --specifier ">=3.44.0.dev"
-then
-  curl --fail-with-body -k -o api.json "${PULP_URL}${PULP_API_ROOT}api/v3/docs/api.json?bindings&component=ansible"
-  USE_LOCAL_API_JSON=1 ./generate.sh pulp_ansible python "$VERSION"
-else
-  ./generate.sh pulp_ansible python "$VERSION"
-fi
+# We need to copy that over to be visible in the container.
+cp "../pulp_ansible/ansible-api.json" .
+./gen-client.sh "ansible-api.json" "ansible" python "pulp_ansible"
 
 pushd pulp_ansible-client
 python setup.py sdist bdist_wheel --python-tag py3
 
-twine check "dist/pulp_ansible_client-$VERSION-py3-none-any.whl"
-twine check "dist/pulp_ansible-client-$VERSION.tar.gz"
+twine check "dist/pulp_ansible_client-"*"-py3-none-any.whl"
+twine check "dist/pulp_ansible-client-"*".tar.gz"
 
-cmd_prefix pip3 install "/root/pulp-openapi-generator/pulp_ansible-client/dist/pulp_ansible_client-${VERSION}-py3-none-any.whl"
-tar cvf ../../pulp_ansible/ansible-python-client.tar ./dist
+tar cvf "../../pulp_ansible/ansible-python-client.tar" ./dist
 
 find ./docs/* -exec sed -i 's/Back to README/Back to HOME/g' {} \;
 find ./docs/* -exec sed -i 's/README//g' {} \;

.github/workflows/scripts/build_ruby_client.sh
@@ -1,0 +1,35 @@
+#!/bin/bash
+
+# This script expects all <app_label>-api.json files to exist in the plugins root directory.
+# It produces a <app_label>-ruby-client.tar file in the plugins root directory.
+
+# WARNING: DO NOT EDIT!
+#
+# This file was generated by plugin_template, and is managed by it. Please use
+# './plugin-template --github pulp_ansible' to update this file.
+#
+# For more info visit https://github.com/pulp/plugin_template
+
+set -mveuo pipefail
+
+# make sure this script runs at the repo root
+cd "$(dirname "$(realpath -e "$0")")"/../../..
+
+pushd ../pulp-openapi-generator
+rm -rf "pulp_ansible-client"
+
+# We need to copy that over to be visible in the container.
+#cp "../pulp_ansible/ansible-api.json" .
+#./gen-client.sh "ansible-api.json" "ansible" ruby "pulp_ansible"
+
+# -------------
+# The generator still needs to have it called api.json at this time...
+cp "../pulp_ansible/api.json" .
+./gen-client.sh "api.json" "ansible" ruby "pulp_ansible"
+# -------------
+
+pushd pulp_ansible-client
+gem build pulp_ansible_client
+tar cvf "../../pulp_ansible/ansible-ruby-client.tar" "./pulp_ansible_client-"*".gem"
+popd
+popd