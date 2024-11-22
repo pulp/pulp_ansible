f47c0125930d0b1c8113e9a7348e7699779ef5cd
Matthias Dellweg <mdellweg@redhat.com>
2024-11-22 13:00:21 +0100

    Fix a regression in migration 0056
    Having more than one collection version in the system prevents the
    migration 0056 to run, because null as a value is not allowed to appear
    more than once.
    
    We need to clean this up very careful, becuase on some systems the
    migration may have applied cleanly.
    
    fixes #2040

--- pulp_ansible/tests/unit/migrations/test_0057_collection_version_sha256_migrate.py
+++ pulp_ansible/tests/unit/migrations/test_0057_collection_version_sha256_migrate.py
@@ -7,33 +7,49 @@ def test_collection_version_sha256_migrate(migrate):
     Collection = apps.get_model("ansible", "Collection")
     CollectionVersion = apps.get_model("ansible", "CollectionVersion")
 
-    artifact = Artifact.objects.create(
-        size=8, sha256="SENTINEL", file=SimpleUploadedFile("foo", b"deadbeef")
+    collection = Collection.objects.create(namespace="snap", name="crackle")
+
+    # Create two collection versions, because `sha256=null` can violate the uniquenes constraint.
+    artifact1 = Artifact.objects.create(
+        size=8, sha256="SENTINEL1", file=SimpleUploadedFile("foo", b"deadbeef")
     )
-    collection = Collection.objects.create(
-        namespace="snap",
-        name="crackle",
-    )
-    cv = CollectionVersion.objects.create(
+    cv1 = CollectionVersion.objects.create(
         pulp_type="collection_version",
         collection=collection,
         namespace="snap",
         name="crackle",
-        version="pop",
+        version="1.0.0",
         version_minor="1",
-        version_major="2",
-        version_patch="3",
+        version_major="0",
+        version_patch="0",
     )
-    cv.contentartifact_set.create(artifact=artifact)
+    cv1.contentartifact_set.create(artifact=artifact1)
+
+    artifact2 = Artifact.objects.create(
+        size=8, sha256="SENTINEL2", file=SimpleUploadedFile("foo", b"beefdead")
+    )
+    cv2 = CollectionVersion.objects.create(
+        pulp_type="collection_version",
+        collection=collection,
+        namespace="snap",
+        name="crackle",
+        version="2.0.0",
+        version_minor="2",
+        version_major="0",
+        version_patch="0",
+    )
+    cv2.contentartifact_set.create(artifact=artifact2)
 
     apps = migrate([("ansible", "0056_collectionversion_sha256")])
     CollectionVersion = apps.get_model("ansible", "CollectionVersion")
 
-    cv = CollectionVersion.objects.get(pk=cv.pk)
-    assert cv.sha256 == ""
+    cv1 = CollectionVersion.objects.get(pk=cv1.pk)
+    assert cv1.sha256 == ""
 
     apps = migrate([("ansible", "0057_collectionversion_sha256_migrate")])
     CollectionVersion = apps.get_model("ansible", "CollectionVersion")
 
-    cv = CollectionVersion.objects.get(pk=cv.pk)
-    assert cv.sha256 == "SENTINEL"
+    cv1 = CollectionVersion.objects.get(pk=cv1.pk)
+    assert cv1.sha256 == "SENTINEL1"
+
+    apps = migrate([("ansible", "0058_fix_0056_regression")])
