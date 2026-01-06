6ca6eedd7c762e0b4122b625146df56de0963c66
pulpbot <pulp-infra@redhat.com>
2026-01-06 16:38:55 +0000

    Bump minor version

--- pulp_ansible/app/__init__.py
+++ pulp_ansible/app/__init__.py
@@ -8,6 +8,6 @@ class PulpAnsiblePluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_ansible.app"
     label = "ansible"
-    version = "0.29.0.dev"
+    version = "0.30.0.dev"
     python_package_name = "pulp-ansible"
     domain_compatible = True
