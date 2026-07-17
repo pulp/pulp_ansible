afd9e765cb68f57aa959fb59c089fd31a54be063
pulpbot <pulp-infra@redhat.com>
2026-07-17 08:32:49 +0000

    Bump version: 0.25.7 → 0.25.8.dev

--- pulp_ansible/app/__init__.py
+++ pulp_ansible/app/__init__.py
@@ -8,5 +8,5 @@ class PulpAnsiblePluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_ansible.app"
     label = "ansible"
-    version = "0.25.7"
+    version = "0.25.8.dev"
     python_package_name = "pulp-ansible"
